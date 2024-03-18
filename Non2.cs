5f0086ffe326cbaf0448b6c14e9e54183fd3dc00
Pramodya Madhushan <148953720+madhushan416@users.noreply.github.com>
2024-03-18 22:26:42 +0530

    Add files via upload
    admin ui update

---
+++ Non2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADMIN
+{
+    public partial class Non2 : Form
+    {
+        public Non2()
+        {
+            InitializeComponent();
+        }
+    }
+}

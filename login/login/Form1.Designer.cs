f7bd913ef00dd8a6cd7fedccf7e0988893b63415
Priyathma <133519888+Priyathma@users.noreply.github.com>
2024-03-18 10:44:25 +0530

    Add files via upload
    login update

--- login/login/Form1.Designer.cs
+++ login/login/Form1.Designer.cs
@@ -31,10 +31,9 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(login));
             this.logo = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,34 +52,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(97, 244);
+            this.button1.Location = new System.Drawing.Point(94, 345);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 49);
             this.button1.TabIndex = 1;
             this.button1.Text = "ADMIN";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(97, 313);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(136, 49);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "STAFF";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.SystemColors.Control;
-            this.button3.Location = new System.Drawing.Point(97, 384);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 49);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "STUDENT";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
@@ -100,13 +78,26 @@
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("KandyUnicode", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(32, 189);
+            this.label2.Location = new System.Drawing.Point(4, 189);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(268, 28);
+            this.label2.Size = new System.Drawing.Size(326, 31);
             this.label2.TabIndex = 5;
             this.label2.Text = "MANAGEMENT SYSTEM";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(138, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "LOGIN";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // login
             // 
@@ -114,10 +105,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(334, 461);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.logo);
             this.MaximizeBox = false;
@@ -135,10 +125,9 @@
 
         private System.Windows.Forms.PictureBox logo;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
